f379da20566c13f0a8229eb995bb368550c6a060
pengcheng.yang <806321554@qq.com>
2016-07-18 21:25:07 +0800

    test

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import bodyParser from "body-parser";
+const port = 2000,
+	app = express(),
+	router = express.Router();
+app.use(bodyParser.urlencoded({
+	extended : 1
+}));
+app.use(express.static(`${__dirname}/resource`, {
+	index : 0,
+	maxAge : 600000
+}));
+app.listen(port);
+console.log(`Server started on port ${port}`);
